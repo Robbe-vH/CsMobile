f7095d677f825e8e599cb2a82f57fd312208f64b
robbe <robbevanhoppe@outlook.com>
2023-09-25 15:12:36 +0200

    Oefening Grid toegeovegd, niet werkend

---
+++ OefeningGrid/OefeningGrid/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OefeningGrid;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ OefeningGrid/OefeningGrid/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OefeningGrid;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
